d321dd60d515b888c010e2756e05ce0d2e3719de
nbkhope <ryumemaru@yahoo.com>
2016-07-18 15:48:40 -0700

    Add PostList component and update PostRow accordingly

--- app.js
+++ app.js
@@ -3,11 +3,29 @@ var PostRow = React.createClass({
     return (
       <li className="collection-item avatar">
         <i className="material-icons circle red">thumb_up</i>
-        <a href="#" className="title">New version of Ruby on Rails released</a>
-        <span className="badge">3 likes</span>
+        <a href={this.props.post.link} className="title">{this.props.post.title}</a>
+        <span className="badge">{this.props.post.likes} likes</span>
       </li>
     );
   }
 });
 
-ReactDOM.render(<PostRow />, document.getElementById('content'));
+var PostList = React.createClass({
+  render: function() {
+    var posts = [
+      { title: "New version of Ruby on Rails released", link: "#", likes: 3 },
+      { title: "JavaScript is the most popular language according to Stack Overflow", link: "#", likes: 2 },
+      { title: "Coding jobs are on demand for 2016", link: "#", likes: 0 }
+    ].map(function(element) {
+      return <PostRow post={element} />
+    });
+
+    return (
+      <ul className="collection">
+        {posts}
+      </ul>
+    );
+  }
+});
+
+ReactDOM.render(<PostList />, document.getElementById('content'));
