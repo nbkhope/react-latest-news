806947a40c9c4fd6b39068da97c496f141862320
nbkhope <ryumemaru@yahoo.com>
2016-07-18 16:12:19 -0700

    Move fake posts DATA up to state of FilterablePostList component; also define getInitialState() for FilterablePostList with filterText set to empty string (is passed as props to SearchBar component)

--- app.js
+++ app.js
@@ -12,11 +12,7 @@ var PostRow = React.createClass({
 
 var PostList = React.createClass({
   render: function() {
-    var posts = [
-      { id: 1, title: "New version of Ruby on Rails released", link: "#", likes: 3 },
-      { id: 2, title: "JavaScript is the most popular language according to Stack Overflow", link: "#", likes: 2 },
-      { id: 3, title: "Coding jobs are on demand for 2016", link: "#", likes: 0 }
-    ].map(function(element) {
+    var posts = this.props.posts.map(function(element) {
       return <PostRow key={element.id} post={element} />
     });
 
@@ -35,7 +31,10 @@ var SearchBar = React.createClass({
         <form>
           <div className="input-field col s12">
             <i className="material-icons prefix">search</i>
-            <input type="text" placeholder="Search..."/>
+            <input
+              type="text"
+              placeholder="Search..."
+              value={this.props.filterText}/>
           </div>
         </form>
       </div>
@@ -44,15 +43,26 @@ var SearchBar = React.createClass({
 });
 
 var FilterablePostList = React.createClass({
+  getInitialState: function() {
+    // Returns the initial state object
+    return {
+      filterText: '', // for the SearchBar component
+      posts: [
+        { id: 1, title: "New version of Ruby on Rails released", link: "#", likes: 3 },
+        { id: 2, title: "JavaScript is the most popular language according to Stack Overflow", link: "#", likes: 2 },
+        { id: 3, title: "Coding jobs are on demand for 2016", link: "#", likes: 0 }
+      ] // for the PostList component
+    };
+  },
   render: function() {
     return (
       <div className="row">
         <div className="col s12 m8 offset-m2">
           <h1>Latest News</h1>
 
-          <SearchBar />
+          <SearchBar filterText={this.state.filterText}/>
 
-          <PostList />
+          <PostList posts={this.state.posts}/>
         </div>
       </div>
     );
