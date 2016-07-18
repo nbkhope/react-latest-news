86a7bce17f46b944ea8326b7749e1311e2a81a68
nbkhope <ryumemaru@yahoo.com>
2016-07-18 16:00:18 -0700

    Add id to post objects; add SearchBar component

--- app.js
+++ app.js
@@ -13,17 +13,32 @@ var PostRow = React.createClass({
 var PostList = React.createClass({
   render: function() {
     var posts = [
-      { title: "New version of Ruby on Rails released", link: "#", likes: 3 },
-      { title: "JavaScript is the most popular language according to Stack Overflow", link: "#", likes: 2 },
-      { title: "Coding jobs are on demand for 2016", link: "#", likes: 0 }
+      { id: 1, title: "New version of Ruby on Rails released", link: "#", likes: 3 },
+      { id: 2, title: "JavaScript is the most popular language according to Stack Overflow", link: "#", likes: 2 },
+      { id: 3, title: "Coding jobs are on demand for 2016", link: "#", likes: 0 }
     ].map(function(element) {
-      return <PostRow post={element} />
+      return <PostRow key={element.id} post={element} />
     });
 
     return (
       <ul className="collection">
         {posts}
       </ul>
+    );
+  }
+});
+
+var SearchBar = React.createClass({
+  render: function() {
+    return (
+      <div className="row">
+        <form>
+          <div className="input-field col s12">
+            <i className="material-icons prefix">search</i>
+            <input type="text" placeholder="Search..."/>
+          </div>
+        </form>
+      </div>
     );
   }
 });
